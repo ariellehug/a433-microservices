08da95b0defc4f6278921453e3e63400af691547
ariellehug <ariellehug@gmail.com>
2025-05-14 14:17:50 +0700

    Initial build and push script

---
+++ build_push_image.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+#Build image dengan tag latest
+docker build -t ghcr.io/ariellehug/a433-microservices/shipping-service:latest .
+
+#Login github pakcages
+echo $CR_PAT | docker login ghcr.io -u ariellehug --password-stdin
+
+#Push image ke github packages
+docker push ghcr.io/ariellehug/a433-microservices/shipping-service:latest
